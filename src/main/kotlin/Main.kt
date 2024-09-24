f18740a679a3356873b9c258310bdcb9375c68ce
Joffrey Bion <joffrey.bion@gmail.com>
2024-09-24 09:44:14 +0200

    Use suspend main() instead of runBlocking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,21 @@
 package org.hildan.leboncoin
 
-import kotlinx.coroutines.runBlocking
-import org.hildan.leboncoin.api.Category
-import org.hildan.leboncoin.api.LbcAd
 import org.hildan.leboncoin.api.LeBonCoin
 import org.hildan.leboncoin.serialization.readLbcAds
 import java.nio.file.Paths
 
 private val IMG_FOLDER = Paths.get("C:", "Users", "joffr", "Desktop", "annonces")
 
-fun main(args: Array<String>) {
+suspend fun main(args: Array<String>) {
     if (args.size < 2) {
         println("Please provide email and password as arguments")
         return
     }
-    runBlocking {
-        val email = args[0]
-        val password = args[1]
-        val session = LeBonCoin().login(email, password)
+    val email = args[0]
+    val password = args[1]
+    val session = LeBonCoin().login(email, password)
 
-        val lbc = readLbcAds(IMG_FOLDER.resolve("annonces2.yml"))
-        println(lbc)
-        lbc.ads.forEach { session.createAd(it) }
-    }
+    val lbc = readLbcAds(IMG_FOLDER.resolve("annonces2.yml"))
+    println(lbc)
+    lbc.ads.forEach { session.createAd(it) }
 }
